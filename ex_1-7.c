f5e5fe8da8936cf65bd23a5912a8a488b6b558ef
ethan <71143458+taurethan@users.noreply.github.com>
2021-03-14 22:38:12 -0400

    Delete ex_1-7.c

--- ex_1-7.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-/* The C Programming Language by Brian W. Kernighan and Dennis M. Ritchie
- * Exercise 1-7. Write a program to print the value of EOF.
- * Solution by ET */
-
-int main() {
-    printf("EOF -> %d\n", EOF);
-}
